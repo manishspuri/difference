c16f1f53cc913bcf0ae189737714056185465993
manish <manish@manish-Inspiron-N5010.(none)>
2013-07-07 15:42:58 +0530

    refactoring code

--- lib/difference.rb
+++ lib/difference.rb
@@ -3,20 +3,23 @@ require "difference/version"
 module Difference
    class ActiveRecord::Base
 	   def differs_from(object,opt={})
-		   return "We cannot compare objects of different classes" if self.class.name != object.class.name
+		   raise "Can't compare objects of different Class." if self.class.name != object.class.name
 
 		   # Set attribute list to compare.
 		   diff={}
-		   attr_list =self.attributes.keys
-		   attr_list= attr_list - opt[:ignore_attributes].to_a if opt[:ignore_attributes].present?
-		   attr_list=  opt[:only_attributes].to_a if opt[:only_attributes].present?
-
-
+		   attr_list = clean_attr(self.attributes.keys)
+		   attr_list = attr_list - clean_attr(opt[:ignore_attributes])
+		   attr_list = attr_list &  clean_attr(opt[:only_attributes]) if opt[:only_attributes].present?
 		   attr_list.each do |attr|
 		     (diff[attr.to_sym] = [self[attr],object[attr]]) if self[attr] != object[attr]
 		   end
 		diff
 	   end
+
+	   private 
+		   def clean_attr(collection)
+		    collection.present? ? collection.to_a.collect{|e| e.to_sym if e.present?}  : []
+		   end
   end
   
 end
